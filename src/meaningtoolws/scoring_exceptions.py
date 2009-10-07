239775a7b63d14f73df39807037046a3bcb0662f
Victor <vic@rhodes.(none)>
2009-10-07 15:38:29 -0300

    Workaround for Invalid API key error response not in a valid JSON format.

--- src/meaningtoolws/scoring_exceptions.py
+++ src/meaningtoolws/scoring_exceptions.py
@@ -1,12 +1,12 @@
 import inspect
 
 def available_exceptions():
-    module= inspect.getmodule(BaseMeaningtoolError)
+    module= inspect.getmodule(ScoringError)
     return [obj for (name, obj) in inspect.getmembers(module) \
-                        if inspect.isclass(obj) and issubclass(obj, BaseMeaningtoolError)]
+                        if inspect.isclass(obj) and issubclass(obj, ScoringError)]
 
-class BaseMeaningtoolError(Exception):
-    message = u"Generic error"
+class ScoringError(Exception):
+    message = u"Generic scoring error"
 
     @classmethod
     def get_code(cls):
@@ -18,7 +18,7 @@ class BaseMeaningtoolError(Exception):
     @staticmethod
     def from_code(code):
         """
-        Returns an exception (subclass of BaseMeaningtoolError) matching the given `code`. 
+        Returns an exception (subclass of ScoringError) matching the given `code`. 
         If no exception found, raises UnknownExceptionCodeError
 
         :parameters:
@@ -33,21 +33,29 @@ class BaseMeaningtoolError(Exception):
         
 class UnknownExceptionCodeError(Exception): pass
 
-class ScoringError(BaseMeaningtoolError): pass
-
 class NoTextScoringError(ScoringError):
     message = u"The object appears to have no relevant text"
 
 class NoClassifiersScoringError(ScoringError):
     message = u"No classifier matches this object"
 
-class MissingComponentsScoringError(BaseMeaningtoolError):
-    # TODO Remove. Shouldn't happen
+class MissingComponentsScoringError(ScoringError):
     message = u"I don't know how to classify (some components are missing)"
 
 class CannotDetectLanguageScoringError(ScoringError):
     message = u"Cannot detect the language of the text"
 
-class UndispatchableRequestScoringError(BaseMeaningtoolError):
-    # TODO Remove. Shouldn't happen
+class UndispatchableRequestScoringError(ScoringError):
     message = u"Could not dispatch this request"
+
+class LimitsExceeded(ScoringError):
+    message = u"API usage limits exceeded"
+
+class ContractNotActive(ScoringError):
+    message = u"Your API key isn't enabled yet"
+
+class UserKeyInvalid(ScoringError):
+    message = u"Invalid API key"
+
+class InvalidArgumentsError(ScoringError):
+    message = u"Some provided arguments are invalid"
